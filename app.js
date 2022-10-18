00f2afdc7f3a4606c32fefb511fc6951e8b34662
lanthien943000 <hongnhat24022000@gmail.com>
2022-10-18 19:21:40 +0700

    update pokemon

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var cors = require("cors");
+require("dotenv").config();
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const exception = new Error(`Path not found`);
+  exception.statusCode = 404;
+  next(exception);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = 401;
+
+  res.status(err.statusCode).send(err.message);
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+const pokemonRouter = require("./pokemon.api.js");
+
+router.use('/pokemons',pokemonRouter); 
+
+router.get('/', function(req, res, next) {
+res.status(200).send("Welcome to Pokemon World")
+});
+
+module.exports = router;
